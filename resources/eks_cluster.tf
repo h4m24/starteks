374105aef56fa65bce4ef0ef07ad9076b7faee18
h4m24 <eng.hfaouri@gmail.com>
2020-09-24 09:53:36 +0200

    formatting

--- resources/eks_cluster.tf
+++ resources/eks_cluster.tf
@@ -4,8 +4,10 @@ resource "aws_eks_cluster" "aws_eks" {
 
   vpc_config {
     subnet_ids = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+
     # control plane and nodegroup security groups
     # security_group_ids      = compact([local.cluster_security_group_id])
+    
     endpoint_private_access = true
     endpoint_public_access  = true
   }
